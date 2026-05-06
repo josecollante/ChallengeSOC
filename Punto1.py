7aa10c5d867de93977bd2dd69aacb4d00293a370
josecollante <jose.collante@despegar.com>
2026-05-06 12:49:25 -0300

    Se modifco el archivo Punto1.py para lograr una conexion exitosa con VT utilizando vt-py  y lograr hacer un request del HASH obtenido en el archivo anteriormente parseado

--- Punto1.py
+++ Punto1.py
@@ -1,6 +1,12 @@
 import os
 import json
+import requests
+from dotenv import load_dotenv
 from simple_term_menu import TerminalMenu
+
+load_dotenv()
+
+API_KEY = os.getenv("VT_API_KEY")
 
 def select_file():
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
@@ -29,7 +35,54 @@ def read_json_file(file_path):
     except Exception as e:
         print(f"Ocurrió un error al leer el archivo: {e}")
         return None
-            
+    
+def check_virustotal(hash_value):
+    if not API_KEY:
+        print("La clave de API de VirusTotal no está configurada. Por favor, configure la variable de entorno VT_API_KEY.")
+        return None
+    url = f"https://www.virustotal.com/api/v3/files/{hash_value}"
+    headers = {
+        "x-apikey": API_KEY
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            print("El hash no se encontró en la web de VirusTotal.")
+            return None
+        else:
+            print(f"Error al consultar VirusTotal: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Ocurrió un error al conectarse con VirusTotal: {e}")
+        return None
+    
+def check_results (virus_total_data, hash_value):
+    try:
+        stats = virus_total_data["data"]["attributes"]["last_analysis_stats"]
+        malicious = stats.get("malicious", 0)
+        harmless = stats.get("harmless", 0)
+        suspicious = stats.get("suspicious", 0)
+        Undetected = stats.get("undetected", 0)
+        print(f"\nResultados del análisis de VirusTotal:\n")
+        print(f"Malicious: {malicious}")
+        print(f"Harmless: {harmless}")
+        print(f"Suspicious: {suspicious}")
+        print(f"Undetected: {Undetected}")
+
+        if malicious > 0:
+            print("\nDEFINICIÓN: El HASH del archivo escaneado es potencialmente malicioso.\n")
+        elif suspicious > 0:
+            print("\nDEFINICIÓN: El HASH del archivo escaneado es potencialmente sospechoso.\n")
+        else:
+            print("\nDEFINICIÓN: El HASH del archivo escaneado no muestra indicios de ser malicioso.\n")
+        Link = f"https://www.virustotal.com/gui/file/{hash_value}/detection"
+        print(f"Para más detalles, puede consultar el siguiente enlace: {Link}")
+        
+    except Exception as e:
+        print(f"Ocurrió un error al procesar los resultados de VirusTotal: {e}")
+
 def main():
     selected_file = select_file()
     if not selected_file:
@@ -38,7 +91,10 @@ def main():
     print(f"\nArchivo seleccionado: {selected_file}")
     hash_value = read_json_file(selected_file)
     if hash_value:
-        print(f"Valor de Hash encontrado: {hash_value}")
+        print(f"\nValor de Hash encontrado: {hash_value}")
+        virus_total_data = check_virustotal(hash_value)
+        if virus_total_data:
+            check_results(virus_total_data, hash_value)
     else:
         print("No se encontró un valor de Hash en el archivo seleccionado.")
 
