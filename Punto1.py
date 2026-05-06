53ffdb7ba4060ff39968fd7ce5a84ea04e504fcf
josecollante <jose.collante@despegar.com>
2026-05-06 10:53:47 -0300

    Creacion del archivo Punto1.py Uso de la libreria simple_term_menu para generar un entorno visual y agradable al usuario para y se agrego la accion de encontrar el hash dentro del archivo JSON que el usuario seleccione

---
+++ Punto1.py
@@ -0,0 +1,45 @@
+import os
+import json
+from simple_term_menu import TerminalMenu
+
+def select_file():
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    if not files:
+        print("En el directorio actual NO hay archivos para seleccionar.")
+        return None
+    menu = TerminalMenu(files, title="Seleccione un archivo . JSON:")
+    index_menu = menu.show()
+    if index_menu is None:
+        return None
+    return files[index_menu]
+
+def read_json_file(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        if "targethash" in data:
+            return data["targethash"]
+        else:
+            print("No se encontro una variable de Hash en el archivo seleccionado")
+            return None
+    except json.JSONDecodeError:
+        print("El archivo seleccionado no tiene las propiedades de un JSON.")
+        return None
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+        return None
+            
+def main():
+    selected_file = select_file()
+    if not selected_file:
+        print("No se seleccionó ningún archivo.")
+        return
+    print(f"\nArchivo seleccionado: {selected_file}")
+    hash_value = read_json_file(selected_file)
+    if hash_value:
+        print(f"Valor de Hash encontrado: {hash_value}")
+    else:
+        print("No se encontró un valor de Hash en el archivo seleccionado.")
+
+if __name__ == "__main__":
+    main()
