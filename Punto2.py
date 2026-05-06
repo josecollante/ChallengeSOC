9db0b2274ff914cda7ce75111ef6357fdae53ba4
josecollante <jose.collante@despegar.com>
2026-05-06 15:45:59 -0300

    Modifacion del codigo Punto2.py paseando la informacion para obtener el ClientIP y consumiendo la API de Abuse IPDB

--- Punto2.py
+++ Punto2.py
@@ -6,7 +6,7 @@ from simple_term_menu import TerminalMenu
 
 load_dotenv()
 
-API_KEY = os.getenv("VT_API_KEY")
+API_KEY = os.getenv("ABUSE_IPDB_API_KEY")
 
 def select_file():
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
@@ -23,11 +23,10 @@ def read_json_file(file_path):
     try:
         with open(file_path, 'r') as f:
             data = json.load(f)
-        if "_source" in data and "targethash" in data["_source"]:
-            return data["_source"]["targethash"]
+        if "_source" in data and "httpRequest" in data["_source"] and "clientIp" in data["_source"]["httpRequest"]:
+            return data["_source"]["httpRequest"]["clientIp"]
         else:
-            print("No se encontro una variable de Hash en el archivo seleccionado")
-            #print(data)
+            print("No se encontro una variable de Client IP en el archivo seleccionado")
             return None
     except json.JSONDecodeError:
         print("El archivo seleccionado no tiene las propiedades de un JSON.")
@@ -36,52 +35,64 @@ def read_json_file(file_path):
         print(f"Ocurrió un error al leer el archivo: {e}")
         return None
     
-def check_virustotal(hash_value):
+def check_abuse_ipdb(ip_value):
     if not API_KEY:
-        print("La clave de API de VirusTotal no está configurada. Por favor, configure la variable de entorno VT_API_KEY.")
+        print("La clave de API de AbuseIPDB no está configurada.\n" \
+        "Por favor, configure la variable de entorno VT_API_KEY.")
         return None
-    url = f"https://www.virustotal.com/api/v3/files/{hash_value}"
+    url = "https://api.abuseipdb.com/api/v2/check"
     headers = {
-        "x-apikey": API_KEY
+        "Key": API_KEY,
+        "Accept": "application/json"
+    }
+    params = {
+        "ipAddress": ip_value,
+        "maxAgeInDays": 90
     }
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
             return response.json()
         elif response.status_code == 404:
-            print("El hash no se encontró en la web de VirusTotal.")
+            print("El IP no se encontró en la web de AbuseIPDB.")
             return None
         else:
-            print(f"Error al consultar VirusTotal: {response.status_code}")
+            print(f"Error al consultar AbuseIPDB: {response.status_code}")
             return None
     except Exception as e:
         print(f"Ocurrió un error al conectarse con VirusTotal: {e}")
         return None
     
-def check_results (virus_total_data, hash_value):
+def check_results (abuse_ipdb_data, ip_value):
     try:
-        stats = virus_total_data["data"]["attributes"]["last_analysis_stats"]
-        malicious = stats.get("malicious", 0)
-        harmless = stats.get("harmless", 0)
-        suspicious = stats.get("suspicious", 0)
-        Undetected = stats.get("undetected", 0)
-        print(f"\nResultados del análisis de VirusTotal:\n")
-        print(f"Malicious: {malicious}")
-        print(f"Harmless: {harmless}")
-        print(f"Suspicious: {suspicious}")
-        print(f"Undetected: {Undetected}")
+        info = abuse_ipdb_data["data"]
 
-        if malicious > 0:
-            print("\nDEFINICIÓN: El HASH del archivo escaneado es potencialmente malicioso.\n")
-        elif suspicious > 0:
-            print("\nDEFINICIÓN: El HASH del archivo escaneado es potencialmente sospechoso.\n")
+        ip = info.get("ipAddress", "N/A")
+        score = info.get("abuseConfidenceScore", "N/A")
+        isp = info.get("isp", "N/A")
+        country = info.get("countryCode", "N/A")
+        total_reports = info.get("totalReports", "N/A")
+        last_reported_at = info.get("lastReportedAt", "N/A")
+
+        print(f"\nResultados de AbuseIPDB para la IP: {ip}\n")
+        print(f"Abuse Confidence Score: {score}")
+        print(f"ISP: {isp}")
+        print(f"País: {country}")
+        print(f"Total de Reportes: {total_reports}")
+        print(f"Último Reporte: {last_reported_at}")
+
+        if score >= 80:    
+            print("\nEsta IP tiene un alto nivel de abuso.\n")
+        elif score >= 30:
+            print("\nEsta IP tiene un nivel moderado de abuso.\n")
         else:
-            print("\nDEFINICIÓN: El HASH del archivo escaneado no muestra indicios de ser malicioso.\n")
-        Link = f"https://www.virustotal.com/gui/file/{hash_value}/detection"
+            print("\nEsta IP tiene un bajo nivel de abuso.\n")
+
+        Link = f"https://www.abuseipdb.com/check/{ip_value}"
         print(f"Para más detalles, puede consultar el siguiente enlace:\n {Link}\n\n")
         
     except Exception as e:
-        print(f"Ocurrió un error al procesar los resultados de VirusTotal: {e}")
+        print(f"Ocurrió un error al procesar los resultados de AbuseIPDB: {e}")
 
 def main():
     selected_file = select_file()
@@ -89,14 +100,14 @@ def main():
         print("No se seleccionó ningún archivo.")
         return
     print(f"\nArchivo seleccionado: {selected_file}")
-    hash_value = read_json_file(selected_file)
-    if hash_value:
-        print(f"\nValor de Hash encontrado: {hash_value}")
-        virus_total_data = check_virustotal(hash_value)
-        if virus_total_data:
-            check_results(virus_total_data, hash_value)
+    ip_value = read_json_file(selected_file)
+    if ip_value:
+        print(f"\nValor de IP de origen encontrado: {ip_value}")
+        abuseipdb_data = check_abuse_ipdb(ip_value)
+        if abuseipdb_data:
+            check_results(abuseipdb_data, ip_value)
     else:
-        print("No se encontró un valor de Hash en el archivo seleccionado.")
+        print("No se encontró un valor de IP de origen en el archivo seleccionado.")
 
 if __name__ == "__main__":
     main()
